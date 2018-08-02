7b7f670c3e63708c140d830872f3afb4867c07a2
Nikolay Lositskiy <nlositskiy@ergoserv.com>
2018-08-02 17:19:23 +0300

    Setup mailgun mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,5 +32,14 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         ENV['MAILGUN_DOMAIN'],
+    authentication: 'plain'
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,14 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         ENV['MAILGUN_DOMAIN'],
+    authentication: 'plain'
+  }
 
   config.i18n.fallbacks = true
 
